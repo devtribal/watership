e14f88af0278718f2143f20a49668ae9c5b1d6c3
tribaldev <thetribaldev@gmail.com>
2026-04-14 12:33:57 +0530

    feat: add main layout with task and widget sections

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,26 @@
+function Widget() {
+    return (
+        <div className="widget-area">
 
+        </div>
+    );
+}
+
+function Task() {
+    return (
+        <div className="task-area">
+            <p>Did you do this task today?</p>
+        </div>
+    );
+}
+
+function App() {
+    return (
+        <div className="wrapper" >
+            <Task />
+            <Widget />
+        </div>
+    );
+}
+
+export default App;
