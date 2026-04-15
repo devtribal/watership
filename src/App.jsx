bcb1921c958e0eb0d0a584cfd06d4be9ccf36bb5
devtribal <thetribaldev@gmail.com>
2026-04-15 22:22:03 +0530

    feat: add yes/no buttons

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 function Widget() {
     return (
         <div className="widget-area">
@@ -6,18 +8,34 @@ function Widget() {
     );
 }
 
-function Task() {
+
+
+function Task({ onYesClick, onNoClick }) {
     return (
         <div className="task-area">
-            <p>Did you do this task today?</p>
+            <p className="task-message">Did you do this task today?</p>
+            <div className="task-button-wrapper">
+                <button className="task-button" onClick={onYesClick} >Yes</button>
+                <button className="task-button" onClick={onNoClick} >No</button>
+            </div>
         </div>
     );
 }
 
 function App() {
+    const [taskCompletion, setTaskCompletion] = useState(null);
+
+    function handleYesClick() {
+        setTaskCompletion(true);
+    }
+
+    function handleNoClick() {
+        setTaskCompletion(false);
+    }
+
     return (
         <div className="wrapper" >
-            <Task />
+            <Task onYesClick={handleYesClick} onNoClick={handleNoClick}/>
             <Widget />
         </div>
     );
